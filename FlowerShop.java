0342b990bbe77bff7b3c3d0adeb44924b16ca287
bqistina <149747575+bqistina@users.noreply.github.com>
2024-02-22 11:44:52 +0800

    Add files via upload

---
+++ FlowerShop.java
@@ -0,0 +1,77 @@
+/**
+ * program description
+ * programmer:BQ
+ * Date:4 February 2024
+ */
+import java.util.Scanner;
+
+public class FlowerShop {
+    public static void main(String[] args) {
+        int size = 10;
+        Flower[] flowerArr = new Flower[size];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter Name of Flower: ");
+            String inputName = scanner.nextLine();
+
+            System.out.print("Enter Color: ");
+            String inputColor = scanner.nextLine();
+
+            System.out.print("Enter Price: ");
+            double inputPrice = scanner.nextDouble();
+
+            System.out.print("Enter Quantity: ");
+            int inputQuantity = scanner.nextInt();
+            scanner.nextLine();
+
+            flowerArr[i] = new Flower(inputName, inputColor, inputPrice, inputQuantity);
+        }
+
+        System.out.println("--------- Flower Inventory ---------");
+        for (Flower flower : flowerArr) {
+            System.out.println(flower.toString());
+        }
+
+        double totalValue = 0;
+        for (Flower flower : flowerArr) {
+            totalValue += flower.getPrice() * flower.getQuantity();
+        }
+        System.out.println("Total value of the flower inventory: " + totalValue);
+        
+        //searching code goes here
+        System.out.print("\nEnter the name of the flower to search: ");
+        String flowerName = scanner.nextLine();
+
+        boolean found = false;
+        for (int i = 0; i < flowerArr.length; i++) {
+            if (flowerArr[i].getName().equalsIgnoreCase(flowerName)) {
+                System.out.println("\nFlower Found!");
+                System.out.println(flowerArr[i].toString());
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("\nFlower Not Found!");
+        }
+
+        System.out.print("\nEnter the name of the flower to restock: ");
+        flowerName = scanner.nextLine();
+
+        // Restock code goes here
+        for (int i = 0; i < flowerArr.length; i++) {
+            if (flowerArr[i].getName().equalsIgnoreCase(flowerName)) {
+                System.out.print("Enter the quantity to add: ");
+                int quantity = scanner.nextInt();
+                int newQuantity = quantity + flowerArr[i].getQuantity();
+                flowerArr[i].setQuantity(newQuantity);
+                
+                System.out.println("\n---------Flower Inventory Restock!---------");
+                System.out.println(flowerArr[i].toString());
+                flowerName = scanner.nextLine();
+            }
+        }
+        }
+}
